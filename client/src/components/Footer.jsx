eab819cdcaf62dbd0a890a499ce1946411113741
Chandan Kumar <ck846747@gmail.com>
2025-12-04 18:01:06 +0530

    Project completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -77,9 +77,9 @@ const Footer = () => {
                     <h2 className='text-base font-medium text-gray-800 uppercase'>Contact</h2>
                     <ul className='mt-3 flex flex-col gap-1.5'>
                         <li>1234 Luxury Drive</li>
-                        <li>San Francisco, CA 94107</li>
-                        <li>+1 234 567890</li>
-                        <li>info@example.com</li>
+                        <li>Partap Nagar, Jaipur Rajasthan</li>
+                        <li>+191 8757826768</li>
+                        <li>ck846747@gmail.com</li>
                     </ul>
                 </div>
 
